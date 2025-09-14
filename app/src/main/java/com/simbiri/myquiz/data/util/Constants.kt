1fd7d982cbd4209079da5ee4dcc3968b3f5a4b8e
SimbaSimbiri <simbiriisaacray@gmail.com>
2025-09-14 13:32:52 +0300

    Changed elastic beanstalk base_url to myquiz.simbiriisaacray.com after making an alias in my AWS hosted zone

--- app/src/main/java/com/simbiri/myquiz/data/util/Constants.kt
+++ app/src/main/java/com/simbiri/myquiz/data/util/Constants.kt
@@ -1,7 +1,7 @@
 package com.simbiri.myquiz.data.util
 
 object Constants {
-    const val BASE_URL = "http://myquiz.us-east-1.elasticbeanstalk.com"
+    const val BASE_URL = "http://myquiz.simbiriisaacray.com"
     const val DATABASE = "quiz.db"
     const val QUIZ_TOPIC_TABLE = "quiz_topic"
     const val QUIZ_QUESTION_TABLE = "quiz_question"
